f82a146e2dacb3842ee45b847aefceb83df18ee3
Claudia Adrio <c.adrio@outlook.com>
2025-11-29 20:23:54 +0000

    Functional Navbar

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -1,13 +1,17 @@
 import { Link } from "react-router-dom";
 
+import Logo from "../assets/img/Logo.png";
+
+
+
 export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-expand-lg bg-body-tertiary">
 			<div className="container-fluid">
-				<a className="navbar-brand" href="#">Logo</a>
-				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-					<span className="navbar-toggler-icon"></span>
-				</button>
+				<a className="navbar-brand mx-5" href="#">
+				{/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> */}
+					<img className="logo" src={Logo} alt="Logo Inanna"></img></a>
+				{/* </button> */}
 				<div className="collapse navbar-collapse" id="navbarSupportedContent">
 					<ul className="navbar-nav me-auto mb-2 mb-lg-0">
 						<li className="nav-item">
@@ -19,11 +23,14 @@ export const Navbar = () => {
 					
 					</ul>
 					<form className="d-flex" role="search">
-						<input className="form-control me-2" type="search" placeholder="Búsqueda avanzada" aria-label="Search" />
-						<button className="btn btn-outline-success" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+						<input className="form-control me-2" type="search" placeholder="Búsqueda avanzada" aria-label="Search" class="bi bi-camera"/>
+						
+						
+						<button className="btn btn-outline-success" type="submit"><i className="fa-solid fa-magnifying-glass"></i></button>
+					
 					</form>
 					<div className="nav-item mx-3 d-flex">
-							<a className="nav-link me-3" href="#"><i class="fa-solid fa-pen-to-square me-1"></i>Registro</a>
+							<a className="nav-link me-3" href="#"><i className="fa-solid fa-pen-to-square me-1"></i>Registro</a>
 					
 							<a className="nav-link" href="#"><i class="fa-regular fa-user me-1"></i>Login</a>
 					</div>
