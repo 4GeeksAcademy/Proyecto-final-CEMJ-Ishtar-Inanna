e2a2ea126220769d4a7a0748ede0357a916a7744
Moisés Esquivel <69405573+moisesEL@users.noreply.github.com>
2025-12-01 20:40:09 +0000

    prueba

--- src/front/services/loginServices.js
+++ src/front/services/loginServices.js
@@ -1,53 +1,47 @@
-
-
-
 export const login = async (username, password) => {
+  const resp = await fetch(
+    `https://scaling-invention-6wp99v5gqwgf4vpx-3001.app.github.dev/api/users/login`,
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username, password }),
+    }
+  );
 
-     const resp = await fetch(`http://localhost:3001/api/users/login`, { 
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ username, password }) 
+  if (!resp.ok) throw Error("There was a problem in the login request");
 
-     })
+  if (resp.status === 401) {
+    throw "Invalid credentials";
+  } else if (resp.status === 400) {
+    throw "Invalid email or password format";
+  }
 
-     if(!resp.ok) throw Error("There was a problem in the login request")
+  const data = await resp.json();
 
-     if(resp.status === 401){throw("Invalid credentials")}
+  // Save your token in the localStorage
+  // Also you should set your user into the store using the setItem function
 
-     else if(resp.status === 400){throw ("Invalid email or password format")}
+  localStorage.setItem("jwt-token", data.token);
 
-     const data = await resp.json()
-
-     // Save your token in the localStorage
-     // Also you should set your user into the store using the setItem function
-
-     localStorage.setItem("jwt-token", data.token);
-
-     return data
-
-}
+  return data;
+};
 
 export const getMyTasks = async () => {
+  // Retrieve token from localStorage
+  const token = localStorage.getItem("jwt-token");
+  const resp = await fetch(`http://localhost:3001/api/protected`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + token, // ⬅⬅⬅ authorization token
+    },
+  });
 
-     // Retrieve token from localStorage
-     const token = localStorage.getItem('jwt-token');
-     const resp = await fetch(`http://localhost:3001/api/protected`, {
-
-        method: 'GET',
-        headers: { 
-          "Content-Type": "application/json",
-          'Authorization': 'Bearer ' + token // ⬅⬅⬅ authorization token
-        } 
-
-     });
-
-     if(!resp.ok) {
-          throw Error("There was a problem in the login request")
-     } else if(resp.status === 403) {
-          throw Error("Missing or invalid token");
-     } 
-     console.log(token)
-     return "WORKS"
-
-}
-
+  if (!resp.ok) {
+    throw Error("There was a problem in the login request");
+  } else if (resp.status === 403) {
+    throw Error("Missing or invalid token");
+  }
+  console.log(token);
+  return "WORKS";
+};
