489b4526c43e0f9ba2abf15839805f1c28020728
Eduard Quiroga <edykiro@gmail.com>
2025-12-13 15:28:36 +0000

    Mostrar animales y registro arreglados y totalmente funcionales

--- src/front/pages/public-pages/LostAnimals.jsx
+++ src/front/pages/public-pages/LostAnimals.jsx
@@ -1,17 +1,21 @@
 import { Link } from "react-router-dom";
-import { getAllPetPosts } from "../../services/petPostServices";
+import { getAllPetPosts, deletePetPost } from "../../services/petPostServices";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom"
 import { useLocation } from 'react-router-dom';
 
 export const LostAnimals = () => {
-    //     // Access the global state and dispatch function using the useGlobalReducer hook.
 
     const [petList, setPetList] = useState([])
 
     const testFetchMascotas = async () => {
         const response = await getAllPetPosts()
         setPetList(response.pets)
+    }
+
+        const deleteMascota = async (pet_id) => {
+        const response = await deletePetPost(pet_id)
+        testFetchMascotas()
     }
 
     const enriched = petList.map(p => ({
@@ -21,19 +25,15 @@ export const LostAnimals = () => {
                 .split('|')
                 .map(s => s.split(':', 2))
                 .map(([k, v]) => {
-                    const val = v.includes(';') ? v.split(';') : v || null;
+                    const val = v?.includes(';') ? v.split(';') : (v || null);
                     return [k, val];
                 })
         )
     }));
 
-    console.log("Aqui esta la variable separada", enriched)
-
-
     useEffect(() => { testFetchMascotas() }, [])
 
     const newList = enriched.filter(pets => pets.is_lost == true)
-
 
     return (
         <div className="container">
@@ -45,14 +45,19 @@ export const LostAnimals = () => {
                             <div className="card-body">
                                 <h5 className="card-title">{pets.name}</h5>
                                 <ul>
-                                    <li className="card-text">{pets.details.Especie}</li>
+                                    <li className="card-text">{pets.breed}</li>
                                 </ul>
                                 <ul>
-                                    <li className="card-text">{pets.details.Tamaño}</li>
+                                    <li className="card-text">{pets.details.Tamano}</li>
                                 </ul>
-                                <Link to="/singleanimalview" state={{ id: pets.id }}>
-                                    <p href="#" className="button btn btn-primary">Más información</p>
-                                </Link>
+                                <div>
+                                    <Link to="/singleanimalview" state={{ id: pets.id }}>
+                                        <p href="#" className="button btn btn-primary">Más información</p>
+                                    </Link>
+                                    <button onClick={()=>deleteMascota(pets.id)} className="button btn btn-primary">
+                                        Delete entry
+                                    </button>
+                                </div>
                             </div>
                         </div>
                     </div>
